4615f60561caa00821c4c88e7cf6550d4e364785
dlawjddn <jwl5015@naver.com>
2023-11-26 16:21:45 +0900

    Feat: main page 완성

---
+++ MS_Project/app/src/main/java/com/example/ms_project/InsertFood.java
@@ -0,0 +1,14 @@
+package com.example.ms_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertFood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_food);
+    }
+}
