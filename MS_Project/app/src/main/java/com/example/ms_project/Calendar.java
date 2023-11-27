74c2351a72a9c80bcc068d847745c5235ec97249
dlawjddn <jwl5015@naver.com>
2023-11-27 19:42:05 +0900

    Feat: 배경 사진, 날짜 선택 추가

--- MS_Project/app/src/main/java/com/example/ms_project/Calendar.java
+++ MS_Project/app/src/main/java/com/example/ms_project/Calendar.java
@@ -2,13 +2,44 @@ package com.example.ms_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 public class Calendar extends AppCompatActivity {
+    private Button selectedDateButton;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        selectedDateButton = findViewById(R.id.buttonDatePicker);
+    }
+    public void showDatePickerDialog(View view) {
+        final java.util.Calendar calendar = java.util.Calendar.getInstance();
+        int year = calendar.get(java.util.Calendar.YEAR);
+        int month = calendar.get(java.util.Calendar.MONTH);
+        int day = calendar.get(java.util.Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+                        // 여기서 선택된 날짜를 처리하면 됩니다.
+                        String selectedDate = selectedYear + "-" + (selectedMonth + 1) + "-" + selectedDay;
+                        selectedDateButton.setText(selectedDate);
+                    }
+                },
+                year, month, day
+        );
+        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+        datePickerDialog.show();
     }
 }
