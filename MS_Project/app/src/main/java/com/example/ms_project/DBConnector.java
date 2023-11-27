a48cf3b57ef8fb98a8569a863486349beefda294
dlawjddn <jwl5015@naver.com>
2023-11-27 19:42:45 +0900

    Feat: DB connection 추가

---
+++ MS_Project/app/src/main/java/com/example/ms_project/DBConnector.java
@@ -0,0 +1,72 @@
+package com.example.ms_project;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class DBConnector extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "projects";
+    private static final int DATABASE_VERSION = 1;
+    private static DBConnector dbHelper = null;
+
+    public DBConnector(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public static DBConnector getInstance(Context context) {
+        if (dbHelper == null) {
+            dbHelper = new DBConnector(context);
+        }
+        return dbHelper;
+    }
+
+    // foodlist 테이블 생성 쿼리
+    private static final String CREATE_ORDER_TABLE = "CREATE TABLE foodlist (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "place TEXT," +
+            "image BLOB," +
+            "type TEXT," +
+            "review TEXT," +
+            "date TEXT," +
+            "time TIME," +
+            "price INTEGER);";
+
+    // food 테이블 생성 쿼리
+    private static final String CREATE_FOOD_TABLE = "CREATE TABLE food (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "foodlist_id INTEGER," +
+            "name TEXT," +
+            "calorie NUMERIC(10, 2)," +
+            "FOREIGN KEY (foodlist_id) REFERENCES foodlist (id) ON DELETE CASCADE ON UPDATE CASCADE);";
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_ORDER_TABLE);
+        db.execSQL(CREATE_FOOD_TABLE);
+        Log.d("DB", "생성완료");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS foodlist");
+        db.execSQL("DROP TABLE IF EXISTS food");
+        onCreate(db);
+    }
+    public long insert(ContentValues addValue, String table) {
+        return getWritableDatabase().insert(table, null, addValue);
+    }
+
+    public Cursor query(String table, String[] columns, String selection, String[] selectionArgs,
+                        String groupBy, String having, String orderBy) {
+        return getReadableDatabase().query(table, columns, selection, selectionArgs, groupBy, having, orderBy);
+    }
+
+    public int delete(String whereClause, String[] whereArgs, String table) {
+        return getWritableDatabase().delete(table, whereClause,
+                whereArgs);
+    }
+}
