548fee25cadf7282ebe89624bc5950311fe9f687
dlawjddn <jwl5015@naver.com>
2023-11-29 00:59:38 +0900

    Feat: repository 로직추가

--- MS_Project/app/src/main/java/com/example/ms_project/FoodRepository.java
+++ MS_Project/app/src/main/java/com/example/ms_project/FoodRepository.java
@@ -1,9 +1,11 @@
 package com.example.ms_project;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.example.ms_project.dto.FoodDetail;
 import com.example.ms_project.dto.FoodSimple;
 
 import java.util.ArrayList;
@@ -50,8 +52,46 @@ public class FoodRepository {
         return foodSimples;
 
     }
+    public FoodDetail getFoodDetail(String date, String type) {
+
+        SQLiteDatabase db = dbConnector.getReadableDatabase();
+
+        FoodDetail foodDetail = null;
+
+        // DB 조회 쿼리 작성
+        String[] columns = {"place", "image", "main_menu", "side_menu", "review", "time", "calorie", "cost"};
+
+        // DB 조회 쿼리 작성
+        String selection = "date = ? AND type = ?";
+        String[] selectionArgs = {date, type};
+
+        // 쿼리 실행
+        Cursor cursor = db.query("food", columns, selection, selectionArgs, null, null, null);
 
 
 
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                @SuppressLint("Range") String place = cursor.getString(cursor.getColumnIndex("place"));
+                @SuppressLint("Range") byte[] image = cursor.getBlob(cursor.getColumnIndex("image"));
+                @SuppressLint("Range") String mainMenu = cursor.getString(cursor.getColumnIndex("main_menu"));
+                @SuppressLint("Range") String sideMenu = cursor.getString(cursor.getColumnIndex("side_menu"));
+                @SuppressLint("Range") String review = cursor.getString(cursor.getColumnIndex("review"));
+                @SuppressLint("Range") String time = cursor.getString(cursor.getColumnIndex("time"));
+                @SuppressLint("Range") int calorie = cursor.getInt(cursor.getColumnIndex("calorie"));
+                @SuppressLint("Range") int cost = cursor.getInt(cursor.getColumnIndex("cost"));
+
+                // FoodDetail 객체 생성
+                foodDetail = new FoodDetail(place, image, mainMenu, sideMenu, review, time, calorie, cost);
+            }
+
+            cursor.close();
+        }
+
+        db.close();
+
+
+        return foodDetail;
+    }
 
 }
