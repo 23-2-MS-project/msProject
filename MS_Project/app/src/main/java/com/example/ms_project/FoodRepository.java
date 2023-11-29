f9d61c44e5621286db7605ccf387a594f0703bc8
dlawjddn <jwl5015@naver.com>
2023-11-30 00:13:23 +0900

    Feat: DB 조회 로직 추가

--- MS_Project/app/src/main/java/com/example/ms_project/FoodRepository.java
+++ MS_Project/app/src/main/java/com/example/ms_project/FoodRepository.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.example.ms_project.dto.AnalyzeDTO;
+import com.example.ms_project.dto.FoodAnalyze;
 import com.example.ms_project.dto.FoodDetail;
 import com.example.ms_project.dto.FoodSimple;
 
@@ -92,6 +94,60 @@ public class FoodRepository {
 
 
         return foodDetail;
+    }
+    public AnalyzeDTO getAnalyzeData(String selectedDate){
+        SQLiteDatabase db = dbConnector.getReadableDatabase();
+
+        String[] columns = {"date", "calorie", "cost", "type"};
+
+        // DB 조회 쿼리 작성
+        String selection = "date LIKE ?";
+        String[] selectionArgs = {selectedDate + "%"};
+
+        // 쿼리 실행
+        Cursor cursor = db.query("food", columns, selection, selectionArgs, null, null, null);
+
+        List<FoodAnalyze> datas = new ArrayList<>();
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                @SuppressLint("Range") String date = cursor.getString(cursor.getColumnIndex("date"));
+                @SuppressLint("Range") String type = cursor.getString(cursor.getColumnIndex("type"));
+                @SuppressLint("Range") int calorie = cursor.getInt(cursor.getColumnIndex("calorie"));
+                @SuppressLint("Range") int cost = cursor.getInt(cursor.getColumnIndex("cost"));
+
+                datas.add(new FoodAnalyze(date, type, cost, calorie));
+            }
+
+            cursor.close();
+        }
+        int[] dayOfMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalMonthKcal = 0;
+        int avgMonthKcal = 0;
+        int breakfastCost = 0;
+        int lunchCost = 0;
+        int dinnerCost = 0;
+        int totalMonthCost = 0;
+        int avgMonthCost = 0;
+        int month = 0;
+        for (FoodAnalyze data : datas) {
+            totalMonthKcal += data.getCalorie();
+            if (data.getType().equals("조식")){
+                breakfastCost += data.getCost();
+            } else if (data.getType().equals("중식")) {
+                lunchCost += data.getCost();
+            } else {
+                dinnerCost += data.getCost();
+            }
+            month = Integer.parseInt(data.getDate().substring(5, 7));
+        }
+        totalMonthCost = breakfastCost + lunchCost + dinnerCost;
+        avgMonthCost = totalMonthCost / dayOfMonth[month-1];
+        avgMonthKcal = totalMonthKcal / dayOfMonth[month-1];
+        AnalyzeDTO analyzeDTO = new AnalyzeDTO(totalMonthKcal, avgMonthKcal, breakfastCost, lunchCost, dinnerCost, totalMonthCost, avgMonthCost);
+        db.close();
+
+        return analyzeDTO;
     }
 
 }
